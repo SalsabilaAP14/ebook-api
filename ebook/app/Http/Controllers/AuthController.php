aa0ad19f446cea754d6840d6e043d046b0f828fd
SalsabilaAP14 <3103120206@student.smktelkom-pwt.sch.id>
2022-10-31 11:41:16 +0700

    auth

--- ebook/app/Http/Controllers/AuthController.php
+++ ebook/app/Http/Controllers/AuthController.php
@@ -2,16 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Auth;
+use Validator;
+use App\Models\User;
 
 class AuthController extends Controller
+
 {
-    public function me(){
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check Password
+        if (!$user || !Hash::check($fields['password'], $user->password))
+        return response ([
+            'message' => 'unauthorized'
+        ],401);
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
         return [
-            'NIS'   => 3103120206,
-            'Nama'  => 'Salsabila Amanda Putri',
-            'Phone' => '085842351845',
-            'Class' => 'XII RPL 6'
+            'message' => 'Logged Out'
         ];
     }
 }
+
+
+// {
+//     public function me()
+//     {
+//         return [
+//             'NIS'   => 3103120206,
+//             'Nama'  => 'Salsabila Amanda Putri',
+//             'Phone' => '085842351845',
+//             'Class' => 'XII RPL 6'
+//         ];
+//     }
+// }
